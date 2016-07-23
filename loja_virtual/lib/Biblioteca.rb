cacc32e64b5dbf2998579fe3f786b4840af5aa22
junin17 <carlosarju@gmail.com>
2016-07-23 19:30:48 -0300

    adicionando arquivos

---
+++ loja_virtual/lib/Biblioteca.rb
@@ -0,0 +1,12 @@
+class Biblioteca
+	attr_reader :livros
+	
+	def initialize
+		@livros = {}
+	end
+	
+	def adiciona(livro)
+		@livros[livro.categoria] ||= []
+		@livros[livro.categoria] << livro
+	end
+end
